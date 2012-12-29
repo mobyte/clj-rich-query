6b2220f75c38e6e95d8598df7931ef7f9424395e
Emil Yessenamanov <mobyte@gmail.com>
2012-12-29 14:07:41 +0600

    Migrate to lein2/nrepl.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject crystal.clojure_libs/query_libs "0.2"
+(defproject crystal.clojure_libs/query_libs "0.3"
   :description "Clojure query lib"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.0.4"]]
-  :dev-dependencies [[swank-clojure "1.3.3"]
-                     [com.mysql/jdbc "5.1.5"]]
-  :compile-path  "build"
-  :library-path  "libs"
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
+                                  [org.clojure/tools.nrepl "0.2.0-RC1"]
+                                  [com.mysql/jdbc "5.1.5"]]}}
+  :compile-path "build"
+  :library-path "libs"
   :aot [libs.db.query
         libs.db.gentablecolumns
         libs.db.gencolumnvalues
